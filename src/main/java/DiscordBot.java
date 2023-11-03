d78c6c64508b18bcd132be7ea415210f0c322bd5
user <timurka2303@gmail.com>
2023-11-03 22:58:58 +0500

    Basic Bot Architecture

---
+++ src/main/java/DiscordBot.java
@@ -0,0 +1,24 @@
+/**
+ * Реализует интерфейс Вот для платформы Discord
+ */
+public class DiscordBot implements Bot {
+    @Override
+    public void Initialize() {
+
+    }
+
+    @Override
+    public void Start() {
+
+    }
+
+    @Override
+    public void sendMessage() {
+
+    }
+
+    @Override
+    public String takeMessage() {
+        return null;
+    }
+}
---
+++ src/main/java/TGBot.java
@@ -0,0 +1,24 @@
+/**
+ * Реализует интерфейс Вот для платформы Telegram
+ */
+public class TGBot implements Bot {
+    @Override
+    public void Initialize() {
+
+    }
+
+    @Override
+    public void Start() {
+
+    }
+
+    @Override
+    public void sendMessage() {
+
+    }
+
+    @Override
+    public String takeMessage() {
+        return null;
+    }
+}
